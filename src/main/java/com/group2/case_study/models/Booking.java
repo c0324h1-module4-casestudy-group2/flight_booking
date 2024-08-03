3c89239ed011c1cfa2ed161cec5c8d7ae2a58d23
baoky1999 <kinataru0123@gmail.com>
2024-08-03 11:02:36 +0700

    ky update model entity

---
+++ src/main/java/com/group2/case_study/models/Booking.java
@@ -0,0 +1,54 @@
+package com.group2.case_study.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table (name = "bookings")
+@Data
+public class Booking {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer bookingId;
+
+    @ManyToOne
+    @JoinColumn(name = "userId")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "flightId")
+    private Flight flight;
+
+    @Column
+    private LocalDateTime bookingDate;
+
+    @Column
+    private Integer numberOfSeats;
+
+    @Column
+    private Double totalPrice;
+
+    @Column(length = 50)
+    private String status;
+
+    @Column(length = 100)
+    private String name;
+
+    @Column
+    private Integer age;
+
+    @Column(length = 10)
+    private String gender;
+
+    @Column(length = 50)
+    private String country;
+
+    @Column(length = 6)
+    private String codeBooking;
+}
---
+++ src/main/java/com/group2/case_study/models/Role.java
@@ -0,0 +1,25 @@
+package com.group2.case_study.models;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "roles",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "APP_ROLE_UK", columnNames = "name") })
+@Data
+public class Role {
+
+    //    ROLE_ADMIN
+//    ROLE_USER
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 50)
+    private String name;
+
+    @Column(nullable = false, length = 50)
+    private String description;
+}
