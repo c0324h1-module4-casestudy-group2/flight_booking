c92a9c5c4857eb9dd2cdd34f17941de91206e626
trantheanh3101 <trantheanhk11@gmail.com>
2024-08-05 15:42:45 +0700

    search

---
+++ src/main/java/com/group2/case_study/services/IAirportService.java
@@ -0,0 +1,10 @@
+package com.group2.case_study.services;
+
+import com.group2.case_study.models.Airport;
+
+import java.util.List;
+
+public interface IAirportService {
+
+    List<Airport> findAll();
+}
