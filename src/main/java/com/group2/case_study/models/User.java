e9ce802e506642eeffae63068a0ba67ed2097b79
daohuyhoang <71455374+daohuyhoang@users.noreply.github.com>
2024-08-05 14:30:40 +0700

    Revert "update login"

--- src/main/java/com/group2/case_study/models/User.java
+++ src/main/java/com/group2/case_study/models/User.java
@@ -1,95 +1,47 @@
 package com.group2.case_study.models;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Entity
+@Table(name = "users",
+        uniqueConstraints = {
+        @UniqueConstraint(name = "APP_USER_UK", columnNames = "username") })
 @Data
-@Table(name = "users")
 public class User {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", nullable = false)
     private Integer id;
 
-    @ManyToMany(fetch = FetchType.LAZY)
-    @JoinTable(name = "users_roles",
-            joinColumns = @JoinColumn(name = "user_id"),
-            inverseJoinColumns = @JoinColumn(name = "role_id"))
-    @OnDelete(action = OnDeleteAction.CASCADE)
-    private Set<Role> roles = new HashSet<Role>();
+    @Column(nullable = false, length = 100)
+    private String fullname;
 
-    @NotBlank
-    @Column(name = "fullname", length = 200, nullable = false)
-    private String fullName;
-
-    @NotBlank
-    @Column(name = "username", length = 200, nullable = false)
+    @Column(nullable = false, unique = true, length = 100)
     private String username;
 
-    @NotBlank
-    @Column(name = "password", length = 255, nullable = false)
+    @Column(nullable = false, length = 200)
     private String password;
 
-    @NotBlank
-    @Column(name = "email", length = 50, nullable = false)
+    @Column(nullable = false, unique = true, length = 100)
     private String email;
 
-    @Column(name = "address", length = 255, nullable = false)
+    @Column(length = 200)
     private String address;
 
-    @Column(name = "phone", length = 12, nullable = false)
+    @Column(length = 12)
     private String phone;
 
-    @Column(name = "avatar", columnDefinition = "text")
+    @Column(length = 200)
     private String avatar;
 
-    @Column(name = "activated")
-    private Boolean activated;
+    @Column(nullable = false)
+    private Boolean activated = true;
 
-    @Column(name = "remember_token", length = 255)
+    @Column(length = 200)
     private String rememberToken;
-
-    // Constructors, getters, and setters
-
-    public User() {
-    }
-
-    public User(Integer id, @NotBlank String fullName, @NotBlank String username, @NotBlank String password,
-                @NotBlank String email, String address, String phone, String avatar,
-                Boolean activated, String rememberToken) {
-        super();
-        this.id = id;
-        this.fullName = fullName;
-        this.username = username;
-        this.password = password;
-        this.email = email;
-        this.address = address;
-        this.phone = phone;
-        this.avatar = avatar;
-        this.activated = activated;
-        this.rememberToken = rememberToken;
-    }
-
-    public User(@NotBlank String fullName, @NotBlank String username, @NotBlank String password,
-                @NotBlank String email, String address, String phone, String avatar,
-                Boolean activated, String rememberToken) {
-        super();
-        this.fullName = fullName;
-        this.username = username;
-        this.password = password;
-        this.email = email;
-        this.address = address;
-        this.phone = phone;
-        this.avatar = avatar;
-        this.activated = activated;
-        this.rememberToken = rememberToken;
-    }
 }
