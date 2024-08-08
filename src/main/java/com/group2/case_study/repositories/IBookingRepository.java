ed528b9888cdca5b34dd93b5e72c0f07e8282f72
quangle0309 <quang9999xx.1@gmail.com>
2024-08-08 16:16:51 +0700

    add pageable

--- src/main/java/com/group2/case_study/repositories/IBookingRepository.java
+++ src/main/java/com/group2/case_study/repositories/IBookingRepository.java
@@ -1,6 +1,8 @@
 package com.group2.case_study.repositories;
 
 import com.group2.case_study.models.Booking;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,5 +14,5 @@ public interface IBookingRepository extends JpaRepository<Booking, Integer> {
     @Query(value = "SELECT * FROM bookings b WHERE b.code_booking = :code", nativeQuery = true)
     Booking findBookingByCode(@Param("code") String code);
 
-    List<Booking> findAllByUserId(Integer userId);
+    Page<Booking> findAllByUserId(Integer userId, Pageable pageable);
 }
